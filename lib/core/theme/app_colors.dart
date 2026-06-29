5e8c3cd1e528ed0bdf76c664dc5fbf8c2bf05332
ezzin2003-arch <ezzin2003@gmail.com>
2026-06-29 16:36:49 +0100

    Create app_colors.dart

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const primary = Color(0xFFD4AF37);
+  static const background = Color(0xFF121212);
+  static const surface = Color(0xFF1E1E1E);
+
+  static const success = Color(0xFF4CAF50);
+  static const error = Color(0xFFE53935);
+
+  static const white = Colors.white;
+}
