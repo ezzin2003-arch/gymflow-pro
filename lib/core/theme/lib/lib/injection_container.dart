4cc6939ca26f883b2d3a83ab7bebd2e35369b8b9
ezzin2003-arch <ezzin2003@gmail.com>
2026-06-29 17:07:14 +0100

    Create injection_container.dart

---
+++ lib/core/theme/lib/lib/injection_container.dart
@@ -0,0 +1,10 @@
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initDependencies() async {
+  // TODO: Database
+  // TODO: Repositories
+  // TODO: UseCases
+  // TODO: Blocs
+}
