2ef134246d408b475ac587d407cc60429e48459b
ezzin2003-arch <ezzin2003@gmail.com>
2026-06-29 17:20:48 +0100

    Create gym_button.dart

---
+++ lib/lib/lib/shared/widgets/gym_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import '../../core/theme/app_colors.dart';
+
+class GymButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const GymButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 55,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
