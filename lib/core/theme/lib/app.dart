1ed229de7e596d8c0593ddd4c89d6f67f043cfb1
ezzin2003-arch <ezzin2003@gmail.com>
2026-06-29 17:11:56 +0100

    Update app.dart

--- lib/core/theme/lib/app.dart
+++ lib/core/theme/lib/app.dart
@@ -47,3 +47,32 @@ class SplashScreen extends StatelessWidget {
     );
   }
 }
+import 'package:flutter/material.dart';
+import 'core/router/app_router.dart';
+import 'core/theme/app_colors.dart';
+
+class GymFlowApp extends StatelessWidget {
+  const GymFlowApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+
+      title: 'GymFlow Pro',
+
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.background,
+        useMaterial3: true,
+        colorScheme: const ColorScheme.dark(
+          primary: AppColors.primary,
+          background: AppColors.background,
+          surface: AppColors.surface,
+          error: AppColors.error,
+        ),
+      ),
+
+      routerConfig: AppRouter.router,
+    );
+  }
+}
