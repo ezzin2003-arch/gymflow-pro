87ae4963520695709bc3742f978bf76e494b3705
ezzin2003-arch <ezzin2003@gmail.com>
2026-06-29 17:28:01 +0100

    Create member.dart

---
+++ lib/lib/features/members/presentation/pages/lib/features/members/domain/entities/member.dart
@@ -0,0 +1,19 @@
+class Member {
+  final String id;
+  final String fullName;
+  final String phone;
+  final int age;
+  final double weight;
+  final double height;
+  final DateTime createdAt;
+
+  Member({
+    required this.id,
+    required this.fullName,
+    required this.phone,
+    required this.age,
+    required this.weight,
+    required this.height,
+    required this.createdAt,
+  });
+}
